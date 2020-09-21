1ecf5da709fbf3a85441b38b376466ec8d83a1ca
Himanshu <hudixt@gmail.com>
2020-09-22 01:55:51 +0530

    feat(init): Initialize

---
+++ test/unique-selector-2.test.ts
@@ -0,0 +1,14 @@
+import UnqiueSelector from "../src/unique-selector-2"
+
+/**
+ * Dummy test
+ */
+describe("Dummy test", () => {
+  it("works if true is truthy", () => {
+    expect(true).toBeTruthy()
+  })
+
+  // it("DummyClass is instantiable", () => {
+  //   expect(new UnqiueSelector()).toBeInstanceOf(UnqiueSelector)
+  // })
+})
